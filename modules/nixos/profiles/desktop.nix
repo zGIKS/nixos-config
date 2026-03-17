e0e973dfd2ef06abf7b36e8e20fd022615d13ae1
zGIKS <mateo@giks.net>
2026-03-17 00:50:01 -0500

    feat(sway): add keybindings for workspace 6 and move container to workspace 6 feat(waybar): enhance workspace configuration with sorting and persistent workspaces fix(waybar): update styles and formatting for various modules

--- modules/nixos/profiles/desktop.nix
+++ modules/nixos/profiles/desktop.nix
@@ -4,6 +4,18 @@
   options.myModules.profiles.desktop.enable = lib.mkEnableOption "desktop profile";
 
   config = lib.mkIf config.myModules.profiles.desktop.enable {
+    fonts.packages = with pkgs; [
+      inter
+      material-design-icons
+      nerd-fonts.jetbrains-mono
+    ];
+
+    fonts.fontconfig.defaultFonts = {
+      sansSerif = [ "Inter" "DejaVu Sans" "Noto Sans" ];
+      serif = [ "DejaVu Serif" "Noto Serif" ];
+      monospace = [ "JetBrainsMono Nerd Font" "DejaVu Sans Mono" "Monospace" ];
+    };
+
     environment.systemPackages = with pkgs; [
       bluez
       networkmanager
