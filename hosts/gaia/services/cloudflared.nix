ae3bf6da12336f2e4d85f1a5792f236a6dc46ace
zGIKS <166082577+zGIKS@users.noreply.github.com>
2026-06-09 17:35:53 -0500

    fix: update cloudflared upstream addresses to correct ports

--- hosts/gaia/services/cloudflared.nix
+++ hosts/gaia/services/cloudflared.nix
@@ -9,11 +9,11 @@
     routes = [
       {
         hostname = "clair-api.giks.net";
-        upstream = "http://127.0.0.1:8088";
+        upstream = "http://127.0.0.1:8081";
       }
       {
         hostname = "clair-edge.giks.net";
-        upstream = "http://127.0.0.1:8088";
+        upstream = "http://127.0.0.1:5000";
       }
     ];
     credentialsFile = config.sops.secrets.cloudflaredCredentials.path;
